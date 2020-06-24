dd2968e2bd98c76beb946424a83ee6fa4418eca3
exilete <66711009+exilete@users.noreply.github.com>
2020-06-24 16:27:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var verbs = ["Abdicate", "Adjudicate", "Alienate", "Amplify", "Ascend", "Assimilate", "Avert", 
+"Bleed", 
+"Cripple", 
+"Decompose", "Demolish", "Desecrate", "Disintegrate", "Disobey", 
+"Eliminate", "Emancipate", "Empower", "Eradicate", "Evade", "Eviscerate", "Evolve", "Exile", "Exterminate",
+"Hallucinate",  
+"Illuminate", "Initiate", "Instigate", "Invigorate", 
+"Liberate",  
+"Maim", "Manipulate", 
+"Obliterate", "Oppress", "Orchestrate",
+"Pray", "Protect", "Putrefy",  
+"Redefine", "Renounce", "Radiate", "Retaliate", 
+"Self-destruct", "Shimmer", 
+"Terminate", "Transgress", 
+"Violate", 
+"Worship"];
+
+document.querySelector(".content-title").innerHTML = 
+		verbs[Math.floor(Math.random() * verbs.length)] + " &#11832 DEMONOLOGY";
