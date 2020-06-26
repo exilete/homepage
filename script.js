7cbca027fbf9ee0141e54a2c3216065c87977a6e
exilete <66711009+exilete@users.noreply.github.com>
2020-06-26 17:05:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var verbs = ["Abdicate", "Adjudicate", "Alienate", "Amplify", "Ascend", "Assimilate", "Avert", 
 "Bleed", 
 "Cripple", 
@@ -15,5 +17,68 @@ var verbs = ["Abdicate", "Adjudicate", "Alienate", "Amplify", "Ascend", "Assimil
 "Violate", 
 "Worship"];
 
-document.querySelector(".content-title").innerHTML = 
-		verbs[Math.floor(Math.random() * verbs.length)] + " &#11832 DEMONOLOGY";
+$(".content-title").html( 
+		verbs[Math.floor(Math.random() * verbs.length)] + " &#11832 DEMONOLOGY");
+		
+
+
+$("document").ready(function() {
+	$(".content-title").removeClass("hidden");
+	$(".content").hide().fadeIn(3000);
+	setTimeout(function() {
+	$(".content-youtube").removeClass("hidden");
+	$(".content-youtube").hide().fadeIn(3000);
+	}, 1000);
+	setTimeout(function() {
+	$(".content-spotify").removeClass("hidden");
+	$(".content-spotify").hide().fadeIn(3000);
+	}, 1500);
+	setTimeout(function() {
+	$(".content-reddit").removeClass("hidden");
+	$(".content-reddit").hide().fadeIn(3000);
+	}, 2000);
+	setTimeout(function() {
+	$(".content-mega").removeClass("hidden");
+	$(".content-mega").hide().fadeIn(3000);
+	}, 2500);
+	setTimeout(function() {
+	$(".content-github").removeClass("hidden");
+	$(".content-github").hide().fadeIn(3000);
+	}, 3000);
+});
+
+/*
+
+$("document").ready(function() {
+	$(".content-title").removeClass("hidden");
+	$(".content").hide().fadeIn(3000);
+	iconFade();	
+});
+
+
+function iconFade() {
+	var timeIn = 1000;
+	for (var i=0; i<$(".channels span").length; i++) {
+		$(".channels span")[i].removeClass("hidden");
+		$(".channels span")[i].hide().fadeIn(3000).delay("timeIn");
+		timeIn += 500;
+	}
+}
+
+
+$("document").ready(function() {
+	$(".content-title").removeClass("hidden");
+	$(".content").hide().fadeIn(3000);
+	$(".content-youtube").removeClass("hidden");
+	$(".content-youtube").hide().fadeIn(3000).delay(1000);
+	$(".content-spotify").removeClass("hidden");
+	$(".content-spotify").hide().fadeIn(3000).delay(1500);
+	$(".content-reddit").removeClass("hidden");
+	$(".content-reddit").hide().fadeIn(3000).delay(2000);
+	$(".content-mega").removeClass("hidden");
+	$(".content-mega").hide().fadeIn(3000).delay(2500);
+	$(".content-github").removeClass("hidden");
+	$(".content-github").hide().fadeIn(3000).delay(3000);
+});
+
+*/
